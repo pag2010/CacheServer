88aef7001fea302bdf0004dad1793283e959afbe
Александр Полторацкий <pag.2010@ya.ru>
2018-10-26 19:57:15 +0300

    Изменил handler на роутеры
    Добавил файл config и функции, обслуживающие /autn и /reg

--- main.go
+++ main.go
@@ -12,15 +12,23 @@ import (
 	"encoding/json"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gocraft/web"
 	"github.com/jmoiron/sqlx"
 	"github.com/satori/go.uuid"
 )
+
+const conf = "config.txt"
+
+type Context struct {
+	AdminToken string
+}
 
 type sUser struct {
 	ID      int    `db:"id"`
 	Login   string `db:"login"`
 	Hash    string `db:"hash"`
 	Session string `db:"session"`
+	RoleId  int    `db:"roleId"`
 }
 
 type pAuth struct {
@@ -28,22 +36,166 @@ type pAuth struct {
 	Hash  string `json:"hash"`
 }
 
-var conn, err1 = sqlx.Connect("mysql", "root:mypass@tcp(localhost:3306)/dnd")
+type pReg struct {
+	Login string `json:"login"`
+	Hash  string `json:"hash"`
+	Token string `json:"token"`
+}
+
+type sConfig struct {
+	DBConnect  string `json:"DataBase"`
+	AdminToken string `json:"Token"`
+}
+
+var Conn *sqlx.DB
 
 func main() {
-
+	file, err := ioutil.ReadFile(conf)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+	var config sConfig
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+	lconn, err1 := sqlx.Connect("mysql", config.DBConnect)
+	Conn = lconn
 	if err1 != nil {
 		log.Printf(err1.Error())
 		return
 	}
-
-	http.HandleFunc("/", handler)
-
+	//router := web.New(Context{}).Middleware(web.LoggerMiddleware).Middleware(web.ShowErrorsMiddleware).Middleware((*Context).GetToken).Post("/reg", (*Context).Reg)
+	router := web.New(Context{}).Middleware(web.LoggerMiddleware).Middleware(web.ShowErrorsMiddleware)
+	router.Subrouter(Context{}, "/").Middleware((*Context).GetToken).Post("/reg", (*Context).Reg)
+	router.Subrouter(Context{}, "/").Post("/auth", (*Context).Auth)
+	//router.Post("/auth", (*Context).Auth)
+	/*http.HandleFunc("/auth1", handlerAuth)
+	http.HandleFunc("/reg", handlerReg)
+	http.HandleFunc("/", handler)*/
 	fmt.Println("Запускаемся. Слушаем порт 8080")
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", router)
+
 }
 
+func (c *Context) GetToken(iWrt web.ResponseWriter, iReq *web.Request, next web.NextMiddlewareFunc) {
+	log.Println("Читаю токен")
+	file, err := ioutil.ReadFile(conf)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+	var config sConfig
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+	c.AdminToken = config.AdminToken
+	next(iWrt, iReq)
+	return
+}
+
+func (c *Context) Reg(iWrt web.ResponseWriter, iReq *web.Request) {
+	log.Println("Регистрация")
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(iReq.Body)
+	fmt.Println(buf.String())
+	var newUser pReg
+
+	err := json.Unmarshal(buf.Bytes(), &newUser)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+	if c.AdminToken == newUser.Token {
+		fmt.Println("Началась бд")
+		var user sUser
+		//user := new(sUser)
+		err = Conn.Get(&user, "select * from users where login=?", newUser.Login)
+		/*if err != nil {
+			log.Println(err.Error())
+			return
+		}*/
+		if err != nil && (err.Error() != "sql: no rows in result set") {
+			log.Printf(err.Error())
+			return
+		}
+		//fmt.Println("Нил?")
+
+		if &user == nil {
+			_, err = Conn.Exec("insert into users (login, hash) values (?,?)", newUser.Login, newUser.Hash)
+			if err != nil {
+				log.Printf(err.Error())
+				return
+			}
+			resp, err := json.Marshal(newUser.Login)
+			if err != nil {
+				log.Printf(err.Error())
+				return
+			}
+			_, err = iWrt.Write(resp)
+			return
+
+		}
+		iWrt.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	log.Println("Токены не совпали")
+	iWrt.WriteHeader(http.StatusBadRequest)
+	return
+}
+
+func (c *Context) Auth(iWrt web.ResponseWriter, iReq *web.Request) {
+	lData := new(bytes.Buffer)
+	var user sUser
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(iReq.Body)
+	fmt.Println(buf.String())
+	var p pAuth
+
+	err := json.Unmarshal(buf.Bytes(), &p)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+
+	fmt.Println(p.Login)
+	err = Conn.Get(&user, "select * from users where login=?", p.Login)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+	if user.Hash == p.Hash {
+
+		session, err := uuid.NewV4()
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+		user.Session = session.String()
+		_, err = Conn.Exec("update users set session =? where id=?", user.Session, user.ID)
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+		fmt.Println(user.Session)
+		jsn, err := json.Marshal(user.Session)
+		lData.Write(jsn)
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+	} else {
+		iWrt.WriteHeader(http.StatusForbidden)
+		lData.Write([]byte("{}"))
+	}
+
+	fmt.Fprintln(iWrt, lData)
+}
 func handler(iWrt http.ResponseWriter, iReq *http.Request) {
 	fmt.Println("Request URL: " + iReq.URL.Path)
 	var lGet = iReq.URL.Path[1:]
@@ -76,7 +228,7 @@ func handler(iWrt http.ResponseWriter, iReq *http.Request) {
 			}
 
 			fmt.Println(p.Login)
-			err = conn.Get(&user, "select * from users where login=?", p.Login)
+			err = Conn.Get(&user, "select * from users where login=?", p.Login)
 			if err != nil {
 				log.Printf(err.Error())
 				return
@@ -89,7 +241,7 @@ func handler(iWrt http.ResponseWriter, iReq *http.Request) {
 					return
 				}
 				user.Session = session.String()
-				_, err = conn.Exec("update users set session =? where id=?", user.Session, user.ID)
+				_, err = Conn.Exec("update users set session =? where id=?", user.Session, user.ID)
 				if err != nil {
 					log.Printf(err.Error())
 					return
@@ -103,6 +255,119 @@ func handler(iWrt http.ResponseWriter, iReq *http.Request) {
 	}
 	lData := readFile(lGet) // Считываем файл
 
+	fmt.Fprintln(iWrt, lData)
+}
+
+func handlerAuth(iWrt http.ResponseWriter, iReq *http.Request) {
+	lData := new(bytes.Buffer)
+	if iReq.Method == "POST" {
+		var user sUser
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(iReq.Body)
+		fmt.Println(buf.String())
+		var p pAuth
+
+		err := json.Unmarshal(buf.Bytes(), &p)
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+
+		fmt.Println(p.Login)
+		err = Conn.Get(&user, "select * from users where login=?", p.Login)
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+		if user.Hash == p.Hash {
+
+			session, err := uuid.NewV4()
+			if err != nil {
+				log.Printf(err.Error())
+				return
+			}
+			user.Session = session.String()
+			_, err = Conn.Exec("update users set session =? where id=?", user.Session, user.ID)
+			if err != nil {
+				log.Printf(err.Error())
+				return
+			}
+			fmt.Println(user.Session)
+			jsn, err := json.Marshal(user.Session)
+			lData.Write(jsn)
+			if err != nil {
+				log.Printf(err.Error())
+				return
+			}
+		} else {
+			iWrt.WriteHeader(http.StatusForbidden)
+			lData.Write([]byte("{}"))
+		}
+
+	} else {
+		iWrt.WriteHeader(http.StatusMethodNotAllowed)
+		lData.Write([]byte("{}"))
+	}
+	fmt.Fprintln(iWrt, lData)
+}
+
+func handlerReg(iWrt http.ResponseWriter, iReq *http.Request) {
+	lData := new(bytes.Buffer)
+	if iReq.Method == "POST" {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(iReq.Body)
+		fmt.Println(buf.String())
+		var newUser pReg
+
+		err := json.Unmarshal(buf.Bytes(), &newUser)
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+		file, err := ioutil.ReadFile(conf)
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+		var config sConfig
+		err = json.Unmarshal(file, &config)
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+		fmt.Println(config.AdminToken)
+		if config.AdminToken == newUser.Token {
+			fmt.Println("Началась бд")
+			var user sUser
+			//user := new(sUser)
+			err = Conn.Get(&user, "select * from users where login=?", newUser.Login)
+			if (err != nil) && (err.Error() != "sql: no rows in result set") {
+				log.Printf(err.Error())
+				return
+			}
+			//fmt.Println("Нил?")
+			if err != nil {
+				fmt.Println(err.Error())
+				_, err = Conn.Exec("insert into users (login, hash) values (?,?)", newUser.Login, newUser.Hash)
+				if err != nil {
+					log.Printf(err.Error())
+					return
+				}
+				resp, err := json.Marshal(newUser.Login)
+				if err != nil {
+					log.Printf(err.Error())
+					return
+				}
+				lData.Write(resp)
+			} else {
+				iWrt.WriteHeader(http.StatusBadRequest)
+				lData.Write([]byte("{}"))
+			}
+		}
+	} else {
+		iWrt.WriteHeader(http.StatusMethodNotAllowed)
+		lData.Write([]byte("{}"))
+	}
 	fmt.Fprintln(iWrt, lData)
 }
 
